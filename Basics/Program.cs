f39cb53dcd1607846680817051fb37b0000c77b0
Liseth Cardozo Sejas <cardozoliseth@gmail.com>
2018-03-12 16:30:22 -0700

    add basic functions

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -9,7 +9,27 @@ namespace Basics
     class Program
     {
         static void Main(string[] args)
+        {   
+            // print hello Liseth 
+            Console.WriteLine("Hello there!");            
+            string display = ReverseSentence();
+            Console.WriteLine("your full name reverse looks like this {0}", display);
+            Console.ReadLine();
+        }
+        /* Create a method that takes an imput fron the user
+            then return the input reversed*/
+
+        static string ReverseSentence()
         {
+            Console.WriteLine("what is your first Name");
+            string fname = Console.ReadLine();
+            Console.WriteLine("What is your Last Name");
+            string lName = Console.ReadLine();
+            return fname + lName;
+        }
+       static string Reversed(string name)
+        {
+            return "reversed";
         }
     }
 }
