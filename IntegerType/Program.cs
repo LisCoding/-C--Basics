79580533b9c6d507efad9b6f046fde719d200b54
Liseth Cardozo Sejas <cardozoliseth@gmail.com>
2018-03-12 17:19:46 -0700

    print max int value

---
+++ IntegerType/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxValue = 2147483647;
+            Console.WriteLine("Max value that a integer can hold {0}", maxValue);
+            Console.ReadKey();
+            maxValue += 2;
+            Console.WriteLine("Can we increase it?" + maxValue);
+            //If we increase a Max int it will add up to negative value 
+        }
+    }
+}
