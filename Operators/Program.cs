a3db8b3a1daedf29f3241c9f6ce41ec9baed1eec
Liseth Cardozo Sejas <cardozoliseth@gmail.com>
2018-03-12 22:13:13 -0700

    expereiment with decrement operator

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -20,11 +20,20 @@ namespace Operators
             /*y = x++ + ++x;
              x++ = 4, x = 5
             ++x = 6 */
+            int a = 10;
+            int b = 12;
+            int d = a--;
+            int f = --b;
+
 
             
 
             Console.WriteLine("The value of x is {0} and y is {1}", x, y);
             Console.WriteLine("The value of z is {0} and k is {1}",z, k);
+            Console.ReadLine();
+            Console.WriteLine("What happened if we decrement any guess?");
+            Console.WriteLine("a is " + a + "...b is " + b +
+                "...d is " + d + " f is " + f);
             Console.ReadLine();
         }
     }
